a85fcfc074043f070f8a15ba19794c85235213fb
valchu <valentinpazm@gmail.com>
2015-06-08 15:54:09 -0300

    creación del proyecto en Xcode, integración del modulo ram

---
+++ NES/CPU/CPU/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPU
+//
+//  Created by Valentin Paz Marcolla on 8/6/15.
+//  Copyright (c) 2015 GamePi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
