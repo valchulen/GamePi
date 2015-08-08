0df6914316655948c67d2f000728fa58cd8ed5c6
valchu <valentinpazm@gmail.com>
2015-08-08 18:16:49 -0300

    ahora esta revisado, habría que ver los bytes de flags y revisar los roes

--- Input/Input/Input/main.cpp
+++ Input/Input/Input/main.cpp
@@ -14,13 +14,16 @@ using namespace std;
 
 ifstream* archivo = NULL;
 
-string read (ifstream* v, int size) {
-    string s = "";
-    for(int i = 0; i < size; i++) s += v->get();
-    return s;
+u8* read (ifstream* v, int size) {
+    u8 val[size];
+    for(int i = 0; i < size; i++) val[i] = v->get();
+    return val; //Si se usa, hay que liberarlo despues
+}
+void skip (ifstream* v, int size) {
+    for(int i = 0; i < size; i++) v->get();
 }
 
-char read (ifstream* v) {
+u8 read (ifstream* v) {
     return v->get();
 }
 
@@ -34,19 +37,20 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     cout<<"Se abrio el archivo "<<n_archivo<<endl;
-    if ( read(archivo, 3) == "NES") {
-        u8 n_16_rom = read(archivo); //ojo que es el 4, y creo que ahora corresponde con el 3
-        u8 n_8_rom = read(archivo);
-        u8 cositas_1 = read(archivo),
-            cositas_2 = read(archivo);
-        u8 n_8_ram = read(archivo);
+        skip(archivo, 3);//corresponde con el "NES", lo leo porque paja convertir de u8 a string
+    //el header deberia tene siempre 16 bytes
+        skip(archivo, 2); //skipeo dos porqye tiene " " + el EOF de ms
+        u8 prg_16_rom = read(archivo);
+        u8 chr_8_rom = read(archivo); //es de la PPU-ROM
+        u8 flags_6 = read(archivo),
+            flags_7 = read(archivo);
+        u8 prg_8_ram = read(archivo); //no se usa en donkey kong
         u8 cositas_3 = read(archivo);
-    
-        while (!archivo->eof()) {
-            cout<<(char)archivo->get();
-        }
-    } else cout<<"Archivo no interpretable";
-    cout<<endl;
+        skip(archivo, 5);
+    u8* trainer;
+    if (flags_6 & 0x04) trainer = read(archivo, 512);
+        u8* prg_rom = read(archivo, 16384 * prg_16_rom);
+    u8* chr_rom = read(archivo, 8192 * chr_8_rom);
     //Destructor de archivo;
     archivo->close();
     return 0;
